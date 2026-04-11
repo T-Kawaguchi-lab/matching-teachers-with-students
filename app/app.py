f5346540737d810cc5fbe8e545f7e831089d51cc
Takuma <tabear.river178@gmail.com>
2026-04-11 18:09:42 +0900

    update

--- app/app.py
+++ app/app.py
@@ -545,7 +545,7 @@ def main() -> None:
         st.markdown("</div>", unsafe_allow_html=True)
 
     except Exception as e:
-        st.error("表示エラー")
+        st.error("表示のエラー")
         st.exception(e)
 
 
